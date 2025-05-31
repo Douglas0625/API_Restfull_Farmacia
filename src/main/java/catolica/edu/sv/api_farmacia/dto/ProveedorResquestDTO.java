19bd12ecd0ea2a16f7ba83d1a4f22b98e1e04de8
luisa <cruzmolina193@gmail.com>
2025-05-31 17:27:17 -0600

    DTO en Proveedor

---
+++ src/main/java/catolica/edu/sv/api_farmacia/dto/ProveedorResquestDTO.java
@@ -0,0 +1,13 @@
+package catolica.edu.sv.api_farmacia.dto;
+
+import lombok.Data;
+
+@Data
+
+public class ProveedorResquestDTO {
+
+    private long idproveedor;
+    private String nombre;
+    private String correo;
+    private String telefono;
+}
